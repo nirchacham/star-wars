27cd4d19310853a39f833f57adf49a4d5b5506dd
nirchacham <nircha11@gmail.com>
2023-02-09 00:26:25 +0200

    code updated

---
+++ api/index.js
@@ -0,0 +1,12 @@
+
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv'
+import app from '../app.js';
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_KEY)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
