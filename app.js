59ae212876cf4531a6b9566aef8043c456358336
nirchacham <nircha11@gmail.com>
2023-02-09 00:49:31 +0200

    code deploy

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/star-wars/users',userRoutes);
 app.use('/star-wars/movies',moviesRoutes);
+app.get('/',async (req,res)=>res.send('Success!'))
 
 export default app;
