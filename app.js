a863ac6cb0e40cc5190c482ea1b5a73205b3621c
nirchacham <nircha11@gmail.com>
2023-02-09 01:07:16 +0200

    code deploy version 2

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import userRoutes from './src/routes/users/star-wars-users.js';
 import moviesRoutes from './src/routes/movies/star-wars-routes.js';
 
 const app = express();
 app.use(cors())
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(express.json())
 app.use('/star-wars/users',cors(),userRoutes);
 app.use('/star-wars/movies',cors(),moviesRoutes);
